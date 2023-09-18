5f6a03029f0cf0c151a086f43c5d3967e5746c09
aesandino <anabellasandino@gmail.com>
2023-09-17 21:40:16 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,7 +12,13 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.
 
+
+
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("newFibonacci", () => {
+  it("returns an array that applies the Fibonacci sequence to each number", () => {
+    expect(greeter()).toEqual("[3, 6, 9, 15, 24, 39])
 
 const fibonacciLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -22,12 +28,25 @@ const fibonacciLength2 = 10
 
 // b) Create the function that makes the test pass.
 
+const newFibonacci = () => {
+  return [3, 6, 9, 15, 24, 39]
+
+// NOTES: I'm going to reach out for help to better understand this, because this is most likely incorrect. 
+
 // Pseudo code:
+// input: number greater than 2
+// output: an array that takes the first number, adds it to itself and then continuosly repeats the process
+// process: Maybe to use "length" as a property and hold the numbers within the component. 
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("greeter", () => {
+  it("returns an array of values sorted from least to greatest", () => {
+    expect(greeter(combinedTimeA)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(greeter(cominedTimeB)).toEqual([10, 15, 20, 45, 60, 65, 100])
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -53,11 +72,30 @@ const studyMinutesWeek2 = {
 
 // b) Create the function that makes the test pass.
 
+const combinedTime = {
+  A: [15, 15, 20, 30, 30, 60, 90],
+  B: [10, 15, 20, 45, 60, 65, 100],
+};
+
+console.log(Object.values(combinedTime));
+
 // Pseudo code:
+// input: an object
+// output: an array of the values sorted in order from small to largest.
+// process: use expect statements to sort the numbers from an object to an array - and maintain the order of small to large in the output
+
+// NOTES: I'm going to reach out for help to better understand this, because this is most likely incorrect. 
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+ describe("greeter", () => {
+  it("returns an array of accumulating sum", () => {
+    expect(greeter(accountTransactions1)).toEqual([100, 83, 60, 51])
+    expect(greeter(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(greeter(accountTransactions3)).toEqual([])
 
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
@@ -70,4 +108,23 @@ const accountTransactions3 = []
 
 // b) Create the function that makes the test pass.
 
+describe("greeter", () => {
+  it("returns an array of accumulating sum", () => {
+
+  
+    let accountTransactions1 = [100, -17, -23, -9]
+    let accountTransactions2 = [250, -89, 100, -96]
+    let accountTransactions3 = []
+
+
+    expect(greeter(accountTransactions1, accountTransactions2, accountTransaction3)).toEqual([100, 83, 60, 51], [250, 161, 261, 165], [])
+  })
+})
+ 
+
 // Pseudo code:
+// input: an array 
+// output: an array that adds the following number to the previous number
+// process: Use expect statements to create the expected outputs of each individual array
+
+// NOTES: I'm going to reach out for help to better understand this, because this is most likely incorrect. 
--- interview-questions.md
+++ interview-questions.md
@@ -6,42 +6,42 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions: AS
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is the combination of using html and JavaScript. One syntax difference is that html uses "<></>" to seperate sections of the code, while Javascript does not. 
 
-Researched answer:
+Researched answer: JSX serves the purpose of being a syntax addition for coders. Specifically, used as an extension to Javascript. JSX outputs react elements so that that the user can experience a more interactive webpage or app. One syntax difference between HTML and JSX is the rule regarding closing tags, each language has opposing requirements. 
 
 2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Typing and entering "yarn" into your terminal allows for your browser to open a tab that displays what you are coding, you can also access the "console" page to view any potential errors. I believe it's the "App.js" file that is modified...?
 
-Researched answer:
+Researched answer: Yarn acts as a dependable delivery service to share and access code with multiple developers. The file that is modified is the script that you define within your package. 
 
 3. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values and props are different because they modify different pieces of code...? 
 
-Researched answer:
+Researched answer: The state value is represented with the code of "setState()" and it's purpose is to hold data pulled from the component. While the "props" are used to modify each piece of data in a certain component, it changes the property of the object it's associated with. 
 
 4. What is the DOM?
 
-Your answer:
+Your answer: The DOM is what creates the user interactive experience. 
 
-Researched answer:
+Researched answer: The abbreviation of DOM represents "Document Model Object", and it is what allows users to enjoy interactive aspects of a webpage. 
 
 5. STRETCH: Which is the difference between a div tag and a span tag?
 
-Your answer:
+Your answer: A div tag and a span tag are different because they seperate different portions of the JSX code...?
 
-Researched answer:
+Researched answer: The "div" tag is used with html and it's purpose is to create seperation between elements within the code. While the "span" tag's purpose is to group the elements within the code. 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: a type of programming that looks for objects that are closely related to eachother. 
 
-2. Ruby:
+2. Ruby: Ruby can be used to create apps, and websites, it's a coding language. 
 
-3. Ruby blocks:
+3. Ruby blocks: Ruby blocks are pieces of functions that can then be used in a method that it's called on. 
